895f1b14f00046c3165c6085b5adfde11d1f26c7
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-12-03 17:07:00 +0800

    Initial Commit

---
+++ sanlian/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sanlian
+//
+//  Created by s20181102929 on 2019/12/3.
+//  Copyright © 2019 s20181102929. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
